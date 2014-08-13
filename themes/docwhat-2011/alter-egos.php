2714933d6e34700297ced14773227d2582979da1
Christian Höltje <docwhat@gerf.org>
2014-08-12 23:38:02 -0400

    updated google plus address

--- themes/docwhat-2011/alter-egos.php
+++ themes/docwhat-2011/alter-egos.php
@@ -61,7 +61,7 @@
       </li>
       <li>
         <a rel="me"
-           href="https://plus.google.com/108719135035895800647/about">Google+</a>
+           href="https://plus.google.com/+ChristianHoltje/about">Google+</a>
       </li>
       <li>
         <a rel="me"
